0c13b819a0b4c6317d8b1ac431143f3335455f01
Sasha Harmaza <sashhrmz@inbox.ru>
2019-10-23 12:23:53 -0300

    fix some bags, but not enough. it works to slowly

--- task_1.h
+++ task_1.h
@@ -12,16 +12,18 @@ void Task_1(std::vector<std::vector<double>>& original_matrix) {
     size_t size = matrix.Size();
     std::vector<std::vector<double>> result_vectors;
     result_vectors.resize(size);
+    double start_time = clock(); // начальное время
+
     for(int j = 0; j < size; ++j) {
-        if (matrix.Get()[j][j] == 0) {
-            std::cout << "there is no inverse matrix" << std::endl;
-            return;
-        }
         result_vectors[j].resize(size);
         for(int i = 0; i < size; ++i) {
             if(i == j) { result_vectors[j][i] = 1; }
         }
     }
+
+    double end_time = clock(); // конечное время
+    double search_time = (end_time - start_time) / CLOCKS_PER_SEC; // искомое время
+    std::cout << " first " <<  search_time << std::endl;
 
     Matrix result_matrix(result_vectors);
 
@@ -31,34 +33,37 @@ void Task_1(std::vector<std::vector<double>>& original_matrix) {
             result_matrix.SwapTwoLines(i, i - 1);
         }
         if(matrix.Get()[i - 1][i] == 0) {
+            if(matrix.Get()[i][i] == 0) {
+                return;
+            }
             continue;
         }
           double factor = (matrix.Get()[i - 1][i]) / (matrix.Get()[i][i]);
-          matrix.LineMultiply(factor, i);
-          result_matrix.LineMultiply(factor, i);
 
-          matrix.LinesSubstraction(i - 1, i);
-          result_matrix.LinesSubstraction(i - 1, i);
+          matrix.LinesSubstraction(i - 1, i, factor);
+          result_matrix.LinesSubstraction(i - 1, i, factor);
     }
 
     for(size_t i = 0; i < matrix.Size(); ++i) {
+        double devider = matrix.Get()[i][i];
+        if (devider == 0) {
+            return;
+        }
         for(size_t j = i + 1; j < matrix.Size(); ++j) {
             if(matrix.Get()[j][i] == 0) {
                 continue;
             }
-            double factor = matrix.Get()[j][i] / matrix.Get()[i][i];
-            matrix.LineMultiply(factor, i);
-            result_matrix.LineMultiply(factor, i);
-
-            matrix.LinesSubstraction(j, i);
-            result_matrix.LinesSubstraction(j, i);
+            double factor = matrix.Get()[j][i] / devider;
+            matrix.LinesSubstraction(j, i, factor);
+            result_matrix.LinesSubstraction(j, i, factor);
         }
         double to_unit = matrix.Get()[i][i];
         result_matrix.DivideElement(to_unit, i, i);
     }
 
-    std::cout << result_matrix;
-    std::cout << matrix;
+
+//    std::cout << result_matrix;
+//    std::cout << matrix;
 }
 
 #endif //LABARATORNAYA1_TASK_1_H
