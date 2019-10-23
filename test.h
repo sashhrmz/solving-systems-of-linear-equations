162a03ef9b8f7bd6465336a0629dc9f402ba779e
Sasha Harmaza <sashhrmz@inbox.ru>
2019-10-23 11:32:22 -0300

    add tests with random matrix in first task

--- test.h
+++ test.h
@@ -5,41 +5,33 @@
 #ifndef LABARATORNAYA1_TEST_H
 #define LABARATORNAYA1_TEST_H
 
+#include <map>
 #include "task_1.h"
 #include "task_2.h"
 
 class Test {
 public:
-    void TestFirstTask() {
-        std::vector<double> row1 =  {9, 3};
-        std::vector<double> row2 =  {4, 9, 3};
-        std::vector<double> row3 =  {4, 5, 5, 3};
-        std::vector<double> row4 =  {3, 5, 2, 3, 2};
-        std::vector<double> row4_ =  {3, 5, 2, 1};
-        std::vector<double> row5 =  {4, 1, 5, 3, 1, 1};
-        std::vector<double> row6 =  {6, 2, 3, 3, 9, 9, 4};
-        std::vector<double> row7 =  {2, 5, 3, 3, 1, 7, 3, 8};
-        std::vector<double> row8 =  {1, 2, 2, 3, 1, 8, 2, 7, 1};
-        std::vector<double> row9 =  {4, 2, 8, 7, 1, 1, 1, 3, 2, 1};
-        std::vector<double> row10 = {2, 1, 8, 4, 1, 4, 1, 5, 3, 9, 5};
-        std::vector<double> row11 = {4, 2, 8, 3, 2, 3, 3, 5, 2, 5, 3, 8};
-        std::vector<double> row12 = {6, 8, 8, 3, 4, 5, 6, 7, 5, 1, 2, 3};
+    std::map<size_t, double> TestFirstTask() {
+        std::map<size_t, double> work_time;
+        for(size_t i = 10; i < 2020; i += 500) {
+            std::vector<std::vector<double>> matrix_vector;
+            double temp;
+            for(size_t j = 0; j < i; ++j) {
+                matrix_vector.emplace_back();
+                for(size_t k = 0; k < std::min((j + 2), i); ++k) {
+                    temp = rand() + 1;
+                    matrix_vector[j].push_back(temp);
+                }
+            }
+            double start_time = clock(); // начальное время
 
-        std::vector<double> row9_ =  {1, 1};
-        std::vector<double> row10_ = {1, 1, 1};
-        std::vector<double> row11_ = {1, 0, 1, 1};
-        std::vector<double> row12_ = {1, 1, 1, 1};
+            Task_1(matrix_vector);
 
-        std::vector<std::vector<double>> matrix = {row1, row2, row3, row4, row5, row6, row7, row8, row9, row10, row11, row12};
-        std::vector<std::vector<double>> matrix1 = {row9_, row10_, row11_, row12_};
-
-        double start_time = clock(); // начальное время
-
-        Task_1(matrix);
-
-        double end_time = clock(); // конечное время
-        double search_time = (end_time - start_time) / CLOCKS_PER_SEC; // искомое время
-        std::cout << search_time;
+            double end_time = clock(); // конечное время
+            double search_time = (end_time - start_time) / CLOCKS_PER_SEC; // искомое время
+            work_time.insert({i, search_time});
+            std::cout << i << " - " << search_time << std::endl;
+        }
     }
 
     void TestSecondTask() {
