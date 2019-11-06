1a52103c158c75681f327e3066313270f079877f
Sasha Harmaza <sashhrmz@inbox.ru>
2019-11-06 05:26:59 -0400

    the other algoritm in task_4

--- task_4.h
+++ task_4.h
@@ -9,24 +9,22 @@
 
 void Task_4(std::vector<std::vector<double>>& matrix, std::vector<double>& values) {
     for(size_t i = 0; i < matrix.size() - 1; ++i) {
-        int j = 2;
-        if (i == 0) {
-            j = 1;
+        values[i] /= matrix[i][0];
+        matrix[i][1] /= matrix[i][0];
+        double t = matrix[i + 1][0];
+        values[i + 1] -= values[i] * t;
+        matrix[i + 1][0] = matrix[i + 1][1] - matrix[i][1] * t;
+        if(i != matrix.size() - 2) {
+            matrix[i + 1][1] = matrix[i + 1][2];
+            matrix[i + 1][2] = 0;
+        } else {
+            matrix[i + 1][1] = 0;
+            values[i + 1] /= matrix[i + 1][0];
         }
-        values[i] /= matrix[i][j - 1];
-        matrix[i][j] /= matrix[i][j - 1];
-        matrix[i + 1][1] -= matrix[i][j] * matrix[i + 1][0];
-        values[i + 1] -= values[i] * matrix[i + 1][0];
     }
     for(int i = static_cast<int>(matrix.size() - 1); i > 0; --i) {
-        if (i == matrix.size() - 1) {
-            values[i] /= matrix[i][1];
-        } else {
-            values[i] -= values[i + 1] * matrix[i][2];
-
-        }
+        values[i - 1] -= values[i] * matrix[i - 1][1];
     }
-    values[0] -= values[1] * matrix[0][1];
 }
 
 #endif //LABARATORNAYA1_TASK_4_H
