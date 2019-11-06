2847ca393cc1ab5e73519c097fd624ca662a058b
Sasha Harmaza <sashhrmz@inbox.ru>
2019-11-06 14:11:49 -0400

    it do permetations

--- task_4.h
+++ task_4.h
@@ -9,13 +9,21 @@
 
 void Task_4(std::vector<std::vector<double>>& matrix, std::vector<double>& values) {
     for(size_t i = 0; i < matrix.size() - 1; ++i) {
+        if(fabs(matrix[i][0]) < fabs(matrix[i + 1][0])) {
+            matrix[i].swap(matrix[i + 1]);
+            std::swap(values[i], values[i + 1]);
+            if(i == matrix.size() - 2) {
+                matrix[i].push_back(0);
+            }
+        }
         values[i] /= matrix[i][0];
         matrix[i][1] /= matrix[i][0];
+        matrix[i][2] /= matrix[i][0];
         double t = matrix[i + 1][0];
         values[i + 1] -= values[i] * t;
         matrix[i + 1][0] = matrix[i + 1][1] - matrix[i][1] * t;
         if(i != matrix.size() - 2) {
-            matrix[i + 1][1] = matrix[i + 1][2];
+            matrix[i + 1][1] = matrix[i + 1][2] - t * matrix[i][2];
             matrix[i + 1][2] = 0;
         } else {
             matrix[i + 1][1] = 0;
@@ -24,6 +32,9 @@ void Task_4(std::vector<std::vector<double>>& matrix, std::vector<double>& value
     }
     for(int i = static_cast<int>(matrix.size() - 1); i > 0; --i) {
         values[i - 1] -= values[i] * matrix[i - 1][1];
+        if(i > 1) {
+            values[i - 2] -= values[i] * matrix[i - 2][2];
+        }
     }
 }
 
